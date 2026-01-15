dafb8b7553dcc3cfd6ad747b283b555d765adcbd
Oleg Latov <oleg@tgadigital.com>
2026-01-15 19:28:35 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+cd deps
+
+if [ ! -d "pas8088" ]; then
+    git clone https://github.com/olatov/pas8088.git
+fi
+
+if [ ! -d "ray4laz" ]; then
+    git clone https://github.com/GuvaCode/ray4laz.git
+    cd ray4laz
+    git checkout 5.0
+    cd ..
+fi
+
+cd ..
+
+docker run \
+    --rm \
+    -v "$(pwd)/src:/src" \
+    -v "$(pwd)/deps/:/deps" \
+    -v "$(pwd)/out:/out" \
+    freepascal/fpc:trunk-bookworm-full \
+    fpc -O2 -CX -XX -Xs /src/poisk.lpr -Fu/deps/pas8088/src -Fu/deps/ray4laz/source -Fl/deps/ray4laz/libs/x86_64-linux -o/out/poisk 
